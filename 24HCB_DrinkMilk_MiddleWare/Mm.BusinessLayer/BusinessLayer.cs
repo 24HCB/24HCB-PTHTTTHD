c2b28387fd1a0aabdd8a98c3175e9173b9bea6a3
TRANHUYEN\huyenttl <ttlehuyen92>
2017-04-16 23:05:19 +0700

    ++add bus by huyen

---
+++ 24HCB_DrinkMilk_MiddleWare/Mm.BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mm.DomainModel;
+using Mm.DataAccessLayer;
+
+namespace Mm.BusinessLayer
+{
+    public class BusinessLayer /*: IBusinessLayer*/
+    {
+        //private readonly IHeadQuaterRepository _hqRepository;
+        //private readonly IBranchRepository _branchRepository;
+
+        //public BusinessLayer()
+        //{
+        //    _hqRepository = new HeadQuaterRepository();
+        //    _branchRepository = new BranchRepository();
+        //}
+
+        //public IList<DomainModel.Department> GetAllDepartment()
+        //{
+        //    return _deptRepository.GetAll();
+        //}
+
+        //public DomainModel.Department GetDepartmentByName(string name)
+        //{
+        //    return _deptRepository.GetSingle(d => d.Name.Equals(name), d => d.Employees);
+        //}
+
+        //public void AddDepartment(params DomainModel.Department[] departments)
+        //{
+        //    _deptRepository.add(departments);
+        //}
+
+        //public void UpdateDepartment(params DomainModel.Department[] departments)
+        //{
+        //    _deptRepository.update(departments);
+        //}
+
+        //public void RemoveDepartment(params DomainModel.Department[] departments)
+        //{
+        //    _deptRepository.remove(departments);
+        //}
+
+        //public IList<DomainModel.Employee> GetEmployeeByDepartmentName(string departmentName)
+        //{
+        //    return _emplRepository.GetList(d => d.Department.Name.Equals(departmentName));
+        //}
+
+        //public void AddEmployee(params DomainModel.Employee[] employees)
+        //{
+        //    _emplRepository.add(employees);
+        //}
+
+        //public void UpdateEmployee(params DomainModel.Employee[] employees)
+        //{
+        //    _emplRepository.update(employees);
+        //}
+
+        //public void RemoveEmployee(params DomainModel.Employee[] employees)
+        //{
+        //    _emplRepository.remove(employees);
+        //}
+    }
+}
---
+++ 24HCB_DrinkMilk_MiddleWare/Mm.BusinessLayer/IBusinessLayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mm.DataAccessLayer;
+using Mm.DomainModel;
+
+namespace Mm.BusinessLayer
+{
+    public interface IBusinessLayer
+    {
+        // HeadQuater
+        IList<TRUSO> GetAllHeadQuater();
+        TRUSO GetTruSoByName(string name);
+        void AddTruSo(params TRUSO[] TruSo);
+        void UpdateTruSo(params TRUSO[] TruSo);
+        void RemoveTruSo(params TRUSO[] TruSo);
+
+        // Branch
+        IList<COSO> GetBranchByHQName(string name);
+        void AddCoSo(params COSO[] coso);
+        void UpdateCoSo(params COSO[] coso);
+        void RemoveCoSo(params COSO[] coso);
+
+    }
+}
