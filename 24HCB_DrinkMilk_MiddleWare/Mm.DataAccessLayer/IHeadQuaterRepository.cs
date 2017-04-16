170c7de3169e3967c99960aaa66e87c8542b13b1
John\John Nguyen <minhvuong651@gmail.com>
2017-04-16 22:50:15 +0700

    add dal and edit database

---
+++ 24HCB_DrinkMilk_MiddleWare/Mm.DataAccessLayer/IHeadQuaterRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mm.DomainModel;
+
+namespace Mm.DataAccessLayer
+{
+    public interface IHeadQuaterRepository : IGenericDataRepository<TRUSO> { }
+    public interface IBranchRepository : IGenericDataRepository<COSO> { }
+    public class HeadQuaterRepository : GenericDataRepository<TRUSO>, IHeadQuaterRepository { }
+    public class BranchRepository : GenericDataRepository<COSO>, IBranchRepository { }
+}
