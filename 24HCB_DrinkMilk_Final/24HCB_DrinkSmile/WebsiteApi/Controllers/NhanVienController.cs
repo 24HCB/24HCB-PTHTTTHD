61c77537d83fffc45ca5437684092cee59f32ecc
minhvuong <minhvuong651@gmail.com>
2017-06-11 11:26:07 +0700

    Update Controller

---
+++ 24HCB_DrinkMilk_Final/24HCB_DrinkSmile/WebsiteApi/Controllers/NhanVienController.cs
@@ -0,0 +1,19 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebsiteApi.Controllers
+{
+    public class NhanVienController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<NHANVIEN> Get()
+        {
+            return new BusinessLayer.BusinessLayer().GetAllNhanVien();
+        }
+    }
+}
