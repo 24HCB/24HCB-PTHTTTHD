bf6026d2ab1db286d5c2914c7538810449737e90
minhvuong <minhvuong651@gmail.com>
2017-06-11 11:26:24 +0700

    Update Controller

---
+++ 24HCB_DrinkMilk_Final/24HCB_DrinkSmile/WebsiteApi/Controllers/PhanPhoiController.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebsiteApi.Controllers
+{
+    public class PhanPhoiController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<PHANPHOI> Get()
+        {
+            return new BusinessLayer.BusinessLayer().GetAllPhanPhoi();
+        }
+
+        // GET api/values/5
+        public IEnumerable<PHANPHOI> Get(int MaBonChua)
+        {
+            //return "value";
+            return new BusinessLayer.BusinessLayer().GetPhanPhoiByBonChuaCode(MaBonChua);
+        }
+    }
+}
