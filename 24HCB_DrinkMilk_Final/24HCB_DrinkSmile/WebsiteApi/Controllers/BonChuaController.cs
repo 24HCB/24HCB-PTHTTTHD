3c47ed2d3bf0078645ba2c4674e916aba7981d10
minhvuong <minhvuong651@gmail.com>
2017-06-11 11:24:19 +0700

    Update Controller

---
+++ 24HCB_DrinkMilk_Final/24HCB_DrinkSmile/WebsiteApi/Controllers/BonChuaController.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebsiteApi.Controllers
+{
+    public class BonChuaController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<BONCHUA> Get()
+        {
+            return new BusinessLayer.BusinessLayer().GetAllBonChua();
+        }
+
+        // GET api/values/5
+        public IEnumerable<BONCHUA> Get(int CoSoCode)
+        {
+            //return "value";
+            return new BusinessLayer.BusinessLayer().GetBonChuaByCoSoCode(CoSoCode);
+        }
+    }
+}
